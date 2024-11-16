0afd7110349bd50d21e6c59398ad44f7b145dcf4
Jesusexe <132572812+Jesusexe@users.noreply.github.com>
2024-11-16 17:56:48 -0500

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,17 @@ function toggleMenu() {
     const menu = document.getElementById("menu");
     menu.classList.toggle("show");
   }
+
+  function validarFormulario() {
+    const nombre = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const mensaje = document.getElementById("message").value;
+
+    if (!nombre || !email || !mensaje) {
+        alert("Por favor, llena todos los campos antes de enviar el formulario.");
+        return false; // Evita el envío del formulario
+    }
+
+    alert("¡Mensaje enviado con éxito!");
+    return true; // Permite el envío del formulario
+}
